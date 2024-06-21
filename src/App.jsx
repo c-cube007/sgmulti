9421d0a0a4c1e97db6899c5b83bfba172b402f26
Elijah Shodipo <114405895+ShoElj@users.noreply.github.com>
2024-06-21 17:13:59 +0000

    added music

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import FeatureSection from "./components/FeatureSection";
 import Footer from "./components/Footer";
+import AudioPlayer from "./components/AudioPlayer";
 
 const App = () => {
   return (
     <div>
+      <AudioPlayer />
       <Navbar />
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <HeroSection />
---
+++ src/components/AudioPlayer.jsx
@@ -0,0 +1,19 @@
+// src/components/AudioPlayer.jsx
+import React, { useEffect, useRef } from "react";
+
+const AudioPlayer = () => {
+  const audioRef = useRef(null);
+
+  useEffect(() => {
+    audioRef.current.play();
+  }, []);
+
+  return (
+    <audio ref={audioRef} loop>
+      <source src="/assets/background-music.mp3" type="audio/mpeg" />
+      Your browser does not support the audio element.
+    </audio>
+  );
+};
+
+export default AudioPlayer;
