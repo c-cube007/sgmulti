bc98273265560e4469f9f32ff7774f5fab07e405
shoElj <shodipoelijah2006@gmail.com>
2024-06-21 19:15:58 +0100

    ms

--- src/components/AudioPlayer.jsx
+++ src/components/AudioPlayer.jsx
@@ -1,3 +1,4 @@
+// src/components/AudioPlayer.jsx
 import React, { useEffect, useRef } from "react";
 import backgroundMusic from "../assets/background-music.mp3"; // Import the music file
 
@@ -11,7 +12,6 @@ const AudioPlayer = () => {
   return (
     <audio ref={audioRef} loop>
       <source src={backgroundMusic} type="audio/mpeg" />
-      Your browser does not support the audio element.
     </audio>
   );
 };
