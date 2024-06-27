9bf2c3dba05fb78839aa9cce2039470e685da5db
Elijah Shodipo <114405895+ShoElj@users.noreply.github.com>
2024-06-27 15:31:16 +0000

    Added audio componet

--- src/components/AudioPlayer.jsx
+++ src/components/AudioPlayer.jsx
@@ -1,18 +1,19 @@
 import React, { useEffect, useRef } from "react";
-import backgroundMusic from "../assets/background-music.mp3"; // Import the music file
+import backgroundMusic from "../assets/background-music.mp3"; // Ensure this path is correct
 
 const AudioPlayer = () => {
   const audioRef = useRef(null);
 
   useEffect(() => {
+    console.log("AudioPlayer mounted");
     const playAudio = () => {
+      console.log("Attempting to play audio");
       audioRef.current.play().catch((error) => {
         console.log("Autoplay prevented: ", error);
       });
     };
 
     document.addEventListener("click", playAudio);
-
     return () => {
       document.removeEventListener("click", playAudio);
     };
